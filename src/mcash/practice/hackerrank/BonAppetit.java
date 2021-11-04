8378c8e767d71460c620f3ef0ed3bdeff89d3f14
ale2011 <ashley.le2011@gmail.com>
2021-11-03 21:28:14 -0400

    BonAppetit completed

---
+++ src/mcash/practice/hackerrank/BonAppetit.java
@@ -0,0 +1,49 @@
+package mcash.practice.hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+class BonAppetitResult {
+
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+    public static void bonAppetit(List<Integer> bill, int ignored, int charged) {
+        // Write your code here
+        int total = 0;
+        
+        for(int i = 0; i < bill.size(); i++) {
+            if(i != ignored)
+                total += bill.get(i);
+        }
+        
+        int split = total / 2;
+        
+        if(split == charged)
+            System.out.println("Bon Appetit");
+        else
+            System.out.println(Math.abs(split - charged));
+    }
+
+}
+
+public class BonAppetit {
+    public static void main(String[] args) throws IOException {
+        List<Integer> a = Arrays.asList(new Integer[]{3, 10, 2, 9});
+        
+        BonAppetitResult.bonAppetit(a, 1, 7);
+        
+    }
+}
